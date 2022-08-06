91b621fe51b56d0e0a2f1581bf848b7f542de4e6
Mina Mamdouh <you@example.com>
2022-08-06 06:11:41 +0200

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_localization/controller/app_language.dart';
+
+/// local saved data
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "login".tr,
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "SignUp".tr,
+              style: TextStyle(fontSize: 32),
+            ),
+            GetBuilder<AppLanguage>(
+              init: AppLanguage(),
+              builder: (controller) {
+                return DropdownButton(
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("en"),
+                      value: 'en',
+                    ),
+                    DropdownMenuItem(
+                      child: Text("ar"),
+                      value: 'ar',
+                    ),
+                  ],
+                  value: controller.appLocale,
+                  onChanged: (value) {
+                    controller.changeLanguage(value);
+                    Get.updateLocale(Locale(value));
+                  },
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_localization/home_screen.dart';
+import 'package:get_localization/translations.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      translations: Translation(),
+      locale: Locale('ar'),
+      fallbackLocale: Locale('ar'),
+      home: HomeScreen(),
+    );
+  }
+}
